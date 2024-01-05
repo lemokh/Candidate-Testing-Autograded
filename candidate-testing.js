0e544dd6f7059db096732492cc039bda3a0ebd94
Dan Volosnikov <byndasma@gmail.com>
2024-01-05 17:58:20 -0600

    part one

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,13 +3,20 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
+
+// ("Who was the first American woman in space? ");
+// ("True or false: 5 kilometer == 5000 meters? ");
+// ("(5 + 3)/2 * 10 = ? ");
+// ("Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ");
+// ("What is the minimum crew size for the ISS? ");
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
@@ -18,19 +25,21 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Enter candidate's name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer === correctAnswer) {
+    console.log("CORRECT");
+  } else {
+    console.log("INCORRECT");
+  }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +51,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+  console.log("Hi " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
