4ae0cbd84b526eece896ac102bd90dd2bfa98948
Dan Volosnikov <byndasma@gmail.com>
2024-01-21 11:51:02 -0500

    partThree branch completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,47 +9,41 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-
-
-//TODO: Variables for Part 2
+// TODO: Variables for Part 2
 let questions = [
   "Who was the first American woman in space? ", 
-  "5 kilometer == 5000 meters: true or false: ? ", 
+  "5 kilometer == 5000 meters: True or false? ", 
   "(5 + 3)/2 * 10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 0? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
   "What is the minimum crew size for the ISS? "
 ];
-let correctAnswers = [ "Sally Ride", "true", 40, 8, 3 ];
-
-let candidateAnswers; // = input.question(questions);
-// !! this doesn't allow me to store each answer in an array !!
-// so doing this on line 39 instead via responses array in for loop
-
-let responses = []; // using this instead of: candidateAnswers = input.question(questions);
+let correctAnswers = [ "Sally Ride", "True", 40, 8, 3 ];
+let candidateAnswers = [];
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+  // TODO 1.1b: Ask for candidate's name
   candidateName = input.question("Enter candidate's name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate each question and assign each response as candidateAnswer
   for (i=0; i<questions.length; i++) {
-    responses.push(input.question(questions[i]));
+    candidateAnswers.push(input.question(questions[i]));
   }
-  // console.log(`responses: `, responses);
+  // console.log(`candidateAnswers: `, candidateAnswers);
 }
 
 function gradeQuiz() {
   // PART TWO
- for (i=0; i<responses.length; i++) {
-  console.log(`candidate response: ${responses[i]},\ncorrect answer: ${correctAnswers[i]}`)
- }
+  for (i=0; i<candidateAnswers.length; i++) {
+    console.log(`candidate answer: ${candidateAnswers[i]},\ncorrect answer: ${correctAnswers[i]}`)
+  }
+
   //PART THREE -- TODO 3.2 use this variable to calculate the candidates score
   let numberCorrect = 0;
   
   for (i=0; i<questions.length; i++) {
-    if (responses[i] == correctAnswers[i]) {
+    if (candidateAnswers[i] == correctAnswers[i]) {
       numberCorrect++;
     }
   };
