1a0b747ddb6679304c602fa2d59fdd7d3bf6d85b
Dan Volosnikov <byndasma@gmail.com>
2024-01-23 12:26:23 -0500

    slight debugging

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = [
-  "Who was the first American woman in space?", 
-  " True or false: 5 kilometer == 5000 meters?", 
-  " (5 + 3)/2 * 10 = ?",
-  " Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 
-  " What is the minimum crew size for the ISS? "
+  "Who was the first American woman in space? ", 
+  "true or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride", "True", 40, "Trajectory", 3
@@ -35,7 +35,7 @@ function askQuestion() {
 }
 
 function gradeQuiz() {
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Tells candidate if answered question correctly 
   if (candidateAnswer == correctAnswer) {
     console.log("CORRECT ANSWER");
   } else {
